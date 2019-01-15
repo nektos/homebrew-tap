c6d13f1d7719f11155b45d14ef14e375d3c00051
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-15 10:02:40 -0800

    Brew formula update for act version v0.0.3

--- Formula/act.rb
+++ Formula/act.rb
@@ -1,9 +1,9 @@
 class Act < Formula
   desc "Run GitHub Actions locally"
   homepage "https://github.com/nektos/act"
-  url "https://github.com/nektos/act/releases/download/v0.0.2/act_Darwin_x86_64.tar.gz"
-  version "0.0.2"
-  sha256 "11760f2e4193a0d5146f1f3007f8312040353118a1caafa8ada717dba025c4ed"
+  url "https://github.com/nektos/act/releases/download/v0.0.3/act_Darwin_x86_64.tar.gz"
+  version "0.0.3"
+  sha256 "3ff309011344b83a907dec5ae1614e16f65995acf8c1ce76341afb5b489c2642"
 
   def install
     bin.install "act"
