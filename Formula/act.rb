28ca4e155ef1c169895ed5671e4c72eabdf00201
goreleaserbot <goreleaser@carlosbecker.com>
2020-02-24 20:56:54 -0500

    Brew formula update for act version v0.2.2

--- Formula/act.rb
+++ Formula/act.rb
@@ -2,16 +2,16 @@
 class Act < Formula
   desc "Run GitHub Actions locally"
   homepage "https://github.com/nektos/act"
-  version "0.2.1"
+  version "0.2.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/nektos/act/releases/download/v0.2.1/act_Darwin_x86_64.tar.gz"
-    sha256 "5142dbf5bebdc3ae698a7654aed87dd1cac121b6d22be9187a6b029b2a4f5081"
+    url "https://github.com/nektos/act/releases/download/v0.2.2/act_Darwin_x86_64.tar.gz"
+    sha256 "91bcfe71ce9dfc2f42889d08f9f6d01975b65876ee6fdd6010120d18554f1079"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/nektos/act/releases/download/v0.2.1/act_Linux_x86_64.tar.gz"
-      sha256 "61ed1a4cb7417b4ed1619a7dc19a6ceaa0cc22076348fdb937d48ff1a92b4eff"
+      url "https://github.com/nektos/act/releases/download/v0.2.2/act_Linux_x86_64.tar.gz"
+      sha256 "397d9c4a13a906d01a3165800ab4a251c2a1c47482f349c877cfc7a24d458739"
     end
   end
 
