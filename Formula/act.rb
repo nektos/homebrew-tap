0e00609ef432dbfe2fd3206de9b417e5e743444c
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-03 13:54:30 -0400

    Brew formula update for act version v0.2.24

--- Formula/act.rb
+++ Formula/act.rb
@@ -5,32 +5,32 @@
 class Act < Formula
   desc "Run GitHub Actions locally"
   homepage "https://github.com/nektos/act"
-  version "0.2.23"
+  version "0.2.24"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/nektos/act/releases/download/v0.2.23/act_Darwin_x86_64.tar.gz"
-      sha256 "f7a4584d6befb6deb60a0376755b4560e3c3106e5465616a25593271c74b244b"
+      url "https://github.com/nektos/act/releases/download/v0.2.24/act_Darwin_x86_64.tar.gz"
+      sha256 "e086253414ad59dd0d3609d66ed57322e9dae9dd3b315e8df65039f03dc167f9"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/nektos/act/releases/download/v0.2.23/act_Darwin_arm64.tar.gz"
-      sha256 "35f37f6853a23ae8c8991732373aca416731fb5d4e35b6994c17cc47879a7b11"
+      url "https://github.com/nektos/act/releases/download/v0.2.24/act_Darwin_arm64.tar.gz"
+      sha256 "4b8731edb1d336ded575f18a6c883bd81a9ce53858e82d6b2c4de261c857ea37"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/nektos/act/releases/download/v0.2.23/act_Linux_x86_64.tar.gz"
-      sha256 "40a090da8383535b37b70d795ea0805e14a12dad77d0ca46d35f8274db74e324"
+      url "https://github.com/nektos/act/releases/download/v0.2.24/act_Linux_x86_64.tar.gz"
+      sha256 "52b7a9152353e107261a1d1365205baa821cffb4b1aced91ba204abc55d54ee0"
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/nektos/act/releases/download/v0.2.23/act_Linux_armv6.tar.gz"
-      sha256 "4441eaf01e47764016c1a0c7b7facde8cd994bc3e464771e77e350059d35fab7"
+      url "https://github.com/nektos/act/releases/download/v0.2.24/act_Linux_armv6.tar.gz"
+      sha256 "84dbe4ee2f1838f9f0f4613609115f33a0969d6ed531148ecbd91d46bb0e0d82"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/nektos/act/releases/download/v0.2.23/act_Linux_arm64.tar.gz"
-      sha256 "21ab7ec9dca36bc21581834070941eb21713bdec49b25639f5179e86bf6e6d3a"
+      url "https://github.com/nektos/act/releases/download/v0.2.24/act_Linux_arm64.tar.gz"
+      sha256 "9c4f0b49878275148b9a6e7fb29295b3b6472c87673a7328ce02fbc002c94bf6"
     end
   end
 
